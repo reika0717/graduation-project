0568a5b2e20907dfe5a0007abc74569ae98f85b3
reika0717 <31158474+reika0717@users.noreply.github.com>
2017-09-27 01:40:16 +0900

    add babel(fix #18 )
    宿題のやつですー！

--- src/script/top.js
+++ src/script/top.js
@@ -1,27 +1,30 @@
-$('document').ready(function() {
-  var options = { videoId: 'e4Is32W-ppk', start: 3 };
-  $('#wrapper').tubular(options);
-  // f-UGhWj1xww cool sepia hd
-  // 49SKbS7Xwf4 beautiful barn sepia
-});
+		/*header背景の動画*/
+		 $('document').ready(function() {
+  	   var options = { videoId: 'e4Is32W-ppk', start: 3 };
+ 	   $('.wrapper').tubular(options);
+ 	   // f-UGhWj1xww cool sepia hd
+ 	   // 49SKbS7Xwf4 beautiful barn sepia
+	   });
 
 	   $(document).ready(function () {
-	     $('#containerTop').tubular({
+	     $('.header').tubular({
 	       videoId: 'crpiAoM_6rU'
 	     });
 	   });
 
+		 /*スクロールに合わせてコンテンツをfadeInさせる*/
 	   $(window).scroll(function () {
 	     var windowHeight = $(window).height(),
 	       topWindow = $(window).scrollTop();
-	     $('.action').each(function () {
+	     $('.fadeIn').each(function () {
 	       var targetPosition = $(this).offset().top;
 	       if (topWindow > targetPosition - windowHeight + 200) {
-	         $(this).addClass("fadeInDown");
+	         $(this).addClass(".fadeInDown");
 	       }
 	     });
 	   });
 
+		 /*naviから選択された場所にスムーズに移動する*/
 	   $(function () {
 	     // スクロールのオフセット値
 	     var offsetY = 0;
@@ -46,23 +49,24 @@ $('document').ready(function() {
 	     });
 	   });
 
-	   var menuHeight = $("#navi").height();
+		 /*スクロール上下によるnaviの表示非表示*/
+	   var menuHeight = $(".navi").height();
 	   var startPos = 0;
 	   $(window).scroll(function () {
 	     var currentPos = $(this).scrollTop();
 	     if (currentPos > startPos) {
 	       if ($(window).scrollTop() >= 200) {
-	         $("#navi").css("top", "-" + 170 + "px");
+	         $(".navi").css("top", "-" + 170 + "px");
 	               }
 	       }else{
-	       $("#navi").css("top", 0 + "px");
+	       $(".navi").css("top", 0 + "px");
 	     }
 	     startPos = currentPos;
 	       });
 
 	$(function() {
-	   var navi = $('#navi');
-	   var topHeight = $("#containerTop").height();
+	   var navi = $('.navi');
+	   var topHeight = $(".header").height();
 	     navi.hide();
 	   $(window).scroll(function () {
 	     var currentPos = $(this).scrollTop();
