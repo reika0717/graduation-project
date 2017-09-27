a60ff9adf59d84cc7835a9ab0c06e3b3100f0be3
reika0717 <31158474+reika0717@users.noreply.github.com>
2017-09-27 12:12:45 +0900

    add node_mpdules/ to gitignore

--- dist/assets/script/site.js
+++ dist/assets/script/site.js
@@ -1,31 +1,30 @@
-<script type="text/javascript">
+'use strict';
 
-$(function(){
-	$(".group").fancybox();
-	});
+$(function () {
+    $(".group").fancybox();
+});
 
-$(function(){
+$(function () {
     var setElm = $('.pic'),
-    delaySpeed = 50,
-    fadeSpeed = 1500;
+        delaySpeed = 50,
+        fadeSpeed = 1500;
 
-    $(window).load(function(){
+    $(window).load(function () {
         randomShow();
-        function randomShow(){
+        function randomShow() {
             var elmLength = setElm.length,
-            randomSet = Math.floor(Math.random()*elmLength);
+                randomSet = Math.floor(Math.random() * elmLength);
 
-            $(setElm[randomSet]).css({display:'block',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
-            setElm.splice(randomSet,1);
+            $(setElm[randomSet]).css({ display: 'block', opacity: '0' }).animate({ opacity: '1' }, fadeSpeed);
+            setElm.splice(randomSet, 1);
 
             if (elmLength > 0) {
-                setTimeout(function(){randomShow();},delaySpeed);
+                setTimeout(function () {
+                    randomShow();
+                }, delaySpeed);
             } else {
                 return false;
             }
         }
     });
 });
-
-
-</script>
---
+++ src/script/site.js
@@ -0,0 +1,26 @@
+$(function(){
+	$(".group").fancybox();
+	});
+
+$(function(){
+    var setElm = $('.pic'),
+    delaySpeed = 50,
+    fadeSpeed = 1500;
+
+    $(window).load(function(){
+        randomShow();
+        function randomShow(){
+            var elmLength = setElm.length,
+            randomSet = Math.floor(Math.random()*elmLength);
+
+            $(setElm[randomSet]).css({display:'block',opacity:'0'}).animate({opacity:'1'},fadeSpeed);
+            setElm.splice(randomSet,1);
+
+            if (elmLength > 0) {
+                setTimeout(function(){randomShow();},delaySpeed);
+            } else {
+                return false;
+            }
+        }
+    });
+});
