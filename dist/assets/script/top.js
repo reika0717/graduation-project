e677aac5ee85bddcfe357cb6d705ca6fdd2da173
reika hirahara <31158474+reika0717@users.noreply.github.com>
2018-01-23 00:25:41 +0900

    put video-data instead of you-tube-video

--- dist/assets/script/top.js
+++ dist/assets/script/top.js
@@ -1,19 +1,5 @@
 'use strict';
 
-/*header背景の動画*/
-$('document').ready(function () {
-	var options = { videoId: 'e4Is32W-ppk', start: 3 };
-	$('.wrapper').tubular(options);
-	// f-UGhWj1xww cool sepia hd
-	// 49SKbS7Xwf4 beautiful barn sepia
-});
-
-$(document).ready(function () {
-	$('.header').tubular({
-		videoId: 'crpiAoM_6rU'
-	});
-});
-
 /*スクロールに合わせてコンテンツをfadeInさせる*/
 $('.fadeIn').css('visibility', 'hidden').on('inview', function () {
 	$(this).addClass('fadeInDown');
